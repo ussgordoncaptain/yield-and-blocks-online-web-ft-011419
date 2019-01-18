d2d105c33dc6020f71b3d87e98e4c0017e53e6d3
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-18 00:19:47 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,9 +4,13 @@ while ii < array.size
   yield array[ii]
   ii+=1
 end
-
+  return array
 end
 
 # call your method here!
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
